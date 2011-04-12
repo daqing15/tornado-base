12ccf24c8243fbeffbcbf546901b8d3e0e4a0d2b
Alex Michael <am607@imperial.ac.uk>
2011-04-13 00:10:57 +0100

    Create local config script.

---
+++ config/local_config.py
@@ -0,0 +1,26 @@
+##################################################### 
+# Creates an empty local config file.               #
+# Includes dev, test, prod environments.            #
+#                                                   #
+# Author: Alex Michael                              #
+#####################################################
+
+def create_local_config():
+    f = open("config.local", 'w')
+    new_file_contents = \
+        "##########################################\n" + \
+        "# Local config file, use it to configure #\n" + \
+        "# any extra local configuration details, #\n" + \
+        "# or to overwrite any default ones. This #\n" + \
+        "# file is ignored from version control.  #\n" + \
+        "##########################################\n" + \
+        "\n" + \
+        "[dev]\n" + \
+        "\n" + \
+        "[test]\n" + \
+        "\n" + \
+        "[prod]"
+    f.write(new_file_contents)
+    
+if __name__ == "__main__":
+    create_local_config()
